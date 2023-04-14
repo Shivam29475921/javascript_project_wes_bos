9f35ff9cb3673bc5ff7434a95f66770ae9565001
Shivam Das <shivamdas16122002@gmail.com>
2023-04-14 14:58:46 +0530

    chrome extension

--- drum.kit/script.js
+++ drum.kit/script.js
@@ -1,3 +1,8 @@
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
+
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -20,7 +25,3 @@ function removeTransition(e)
     }    
     this.classList.remove('playing');
 }
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-window.addEventListener('keydown', playSound);
